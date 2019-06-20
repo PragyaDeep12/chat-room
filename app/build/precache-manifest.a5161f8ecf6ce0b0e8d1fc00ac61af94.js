c4f3724f0bd7a9ef523c79caa4fb274bd95a20b5
Pragya Deep <pragya.deep19@gmail.com>
2019-06-20 21:25:09 +0530

    site-added
    Signed-off-by: Pragya Deep <pragya.deep19@gmail.com>

---
+++ app/build/precache-manifest.a5161f8ecf6ce0b0e8d1fc00ac61af94.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641f9d4e177ddc826eb0df1ef1410e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "384de0b066c6bf2d4c55",
+    "url": "/static/css/2.6749d01e.chunk.css"
+  },
+  {
+    "revision": "47785ba98f01482cb9e1",
+    "url": "/static/css/main.30237124.chunk.css"
+  },
+  {
+    "revision": "384de0b066c6bf2d4c55",
+    "url": "/static/js/2.165f6992.chunk.js"
+  },
+  {
+    "revision": "47785ba98f01482cb9e1",
+    "url": "/static/js/main.30b182f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bfb96aeaa1637b4395db8680d19ab5c4",
+    "url": "/static/media/angry.bfb96aea.svg"
+  },
+  {
+    "revision": "0072162b8a20227a3ef31a4ae6668997",
+    "url": "/static/media/app-icon.0072162b.svg"
+  },
+  {
+    "revision": "cc58b558926bb239235f7883e6448c2c",
+    "url": "/static/media/background1.cc58b558.jpg"
+  },
+  {
+    "revision": "c30d3d430730caaa2839280897c5481f",
+    "url": "/static/media/confused.c30d3d43.svg"
+  },
+  {
+    "revision": "fe43d80ffae7ab5d2bc9bb63314925b6",
+    "url": "/static/media/happy.fe43d80f.svg"
+  },
+  {
+    "revision": "afa9bc31db603b4e16cd169c3040a2bd",
+    "url": "/static/media/loading.afa9bc31.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a196067dcd8390262d0ed11067b223f1",
+    "url": "/static/media/sad.a196067d.svg"
+  },
+  {
+    "revision": "882b3ad56ff29ce8bedacf9e3b37fc37",
+    "url": "/static/media/scared.882b3ad5.svg"
+  },
+  {
+    "revision": "cce9c06f5a2e72a3b90e2598aeb43aa7",
+    "url": "/static/media/send-button.cce9c06f.svg"
+  },
+  {
+    "revision": "05130f471c7ff4545ba023785e1b7ae0",
+    "url": "/static/media/user.05130f47.svg"
+  }
+]);
