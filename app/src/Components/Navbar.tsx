ec7df395197543019ae4e7b5dde21f3910e5b74a
Pragya Deep <pragya.deep19@gmail.com>
2019-06-21 22:33:33 +0530

    updated ui
    Signed-off-by: Pragya Deep <pragya.deep19@gmail.com>

--- app/src/Components/Navbar.tsx
+++ app/src/Components/Navbar.tsx
@@ -3,15 +3,6 @@ import firebase from "firebase";
 import { socket } from "../Dao/SocketDAO";
 import { openSnackbar } from "./CustomSnackBar";
 export default function Navbar() {
-  let isMounted = false;
-  useEffect(() => {
-    if (!isMounted) {
-      isMounted = true;
-      // socket.on("latestOnlineUsers", onlineUsers => {
-      //   // console.log(onlineUsers);
-      // });
-    }
-  }, []);
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -25,10 +16,9 @@ export default function Navbar() {
           onClick={async () => {
             socket.disconnect();
             await firebase.auth().signOut();
-            // openSnackbar({ message: "Demo Message", timeout: 3000 });
           }}
         >
-          logout
+          LOGOUT
         </button>
       </nav>
     </div>
